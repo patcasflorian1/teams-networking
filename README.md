1df972a44f529f4649e5b340a436c7c76dded6a8
Eurovending <europarstvending@gmail.com>
2023-03-02 20:46:26 +0200

    new

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Teams Networking...
 
 TODO...
+## how to deploy on github
+- npm run deploy
+-git add . && commit -m "new build" && git push
+-commit
+-push
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = env => {
     output: {
       filename: "main.js",
       path: path.resolve(__dirname, "docs"),
-      publicPath: "/"
+      publicPath: ""
     }
   };
 };
