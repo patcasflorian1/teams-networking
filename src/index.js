1ebc361187da86fd26b62553bd9518e3b13b1dd7
Eurovending <europarstvending@gmail.com>
2023-04-06 19:39:07 +0300

    AwaitOk

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { sleep } from "./utilities";
 let allTeams = [];
 let editId;
 
-
-
 function readTeam() {
   return {
     promotion: document.getElementById("promotion").value,
@@ -68,14 +66,38 @@ async function onSubmit(e) {
   const team = readTeam();
   if (editId) {
     team.id = editId;
-    const status = await updateTeamRequest(team); 
+    const status = await updateTeamRequest(team);
+    if (status.success) {
+      // load new teams...?
+      //loadTeams();
+      allTeams = allTeams.map(t => {
+        if (t.id === team.id) {
+          return {
+            ...t,
+            ...team
+          };
+        }
+        return t;
+      });
 
-        displayTeams(allTeams);
-        e.target.reset();
-    
+      displayTeams(allTeams);
+      e.target.reset();
+    }
   } else {
     const status = await createTeamRequest(team);
-    console.warn("status", status.success, status.id);
+    if (status.success) {
+      // 1. adaugam datele in table...
+      //   1.0. adaug id in team
+      team.id = status.id;
+      //   1.1. addaug team in allTeams
+      //allTeams.push(team);
+      allTeams = [...allTeams, team];
+      //   1.2. apelam displayTeams(allTeams);
+      displayTeams(allTeams);
+      // 2. stergem datele din inputuri
+      //writeTeam({ promotion: "", name: "", url: "", members: "" });
+      e.target.reset();
+    }
   }
 }
 
@@ -96,16 +118,11 @@ function initEvents() {
   document.querySelector("#teams tbody").addEventListener("click", async e => {
     if (e.target.matches("a.remove-btn")) {
       const id = e.target.dataset.id;
-      deleteTeamRequest(id).then(status => {
-        if (status.success) {
-          loadTeams();
-        }
-      });
-
       const status = await deleteTeamRequest(id);
-      if (status.success) { 
-      loadTeams();
-    }
+      if (status.success) {
+        loadTeams();
+        // TODO homework: don't load all teams...
+      }
     } else if (e.target.matches("a.edit-btn")) {
       const id = e.target.dataset.id;
       prepareEdit(id);
@@ -116,13 +133,15 @@ function initEvents() {
 loadTeams();
 initEvents();
 
-//TODO move to he external file
-sleep(2000).then(() => {
-  console.warn("done");
+// TODO move in external file
+console.info("sleep");
+sleep(2000).then(r => {
+  console.info("done1", r);
 });
 console.warn("after sleep");
+
 (async () => {
-  console.info("start");
- var r2 =  await sleep(5000);
-  console.warn("done2",r2);
+  console.info("sleep2");
+  var r2 = await sleep(5000);
+  console.warn("done2", r2);
 })();
